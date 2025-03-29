683bb3db33e33e93448c1e83e90fda929ea01d5f
Estevaomotta <estevosilvamotta@hotmail.com>
2025-03-29 18:47:21 +0000

    not

---
+++ not/not.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+    int main() {
+
+        int a = 0;
+        
+        if (!a) {
+            printf("a variavel a e zero. \n");
+        } else {
+            printf("A variavel e diferente de zero. \n");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
